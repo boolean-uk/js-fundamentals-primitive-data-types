2e7d331f1426b36da7000a36b06d2b52ce3757f0
akibria12 <ahkibria@hotmail.co.uk>
2022-09-12 22:31:27 +0100

    submitted

--- src/numbers.js
+++ src/numbers.js
@@ -8,22 +8,22 @@ const numThree = 32
 //       NOT myAnswer = 336
 
 // 1. Set this variable to numOne added to numTwo
-const numOnePlusNumTwo = 24
+const numOnePlusNumTwo = numOne + numTwo
 
 // 2. Set this variable to numThree multiplied by numTwo
-const numThreeTimesNumTwo = 512
+const numThreeTimesNumTwo = numThree * numTwo
 
 // 3. Set this variable to numThree divided by numOne
-const numThreeDividedByNumOne = 4
+const numThreeDividedByNumOne = numThree / numOne
 
 // 4. Set this variable to numThree minus numOne
-const numThreeMinusNumOne = 24
+const numThreeMinusNumOne = numThree - numOne
 
 // 5. Set this variable to the sum of numOne, numTwo and numThree
-const sum = 56
+const sum = numOne + numTwo + numThree
 
 // 6. Set this variable to the sum of (numOne, numTwo, numThree) divided by numOne
-const numBytes = 7
+const numBytes = (numOne + numTwo + numThree) / numOne
 
 // do not edit the exported object.
 module.exports = {
