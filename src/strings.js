b7d1569997eade93814b9dfb275d91856424082c
Oliver West <oliversimon.west@gmail.com>
2022-11-09 15:36:07 +0000

    completed the strings exercise

--- src/strings.js
+++ src/strings.js
@@ -3,7 +3,6 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const firstName = 'Jane'
 const secondName = 'Smith'
 
-console.log(alphabet, firstName, secondName)
 
 // TODO: Replace null in the lines below using Javascript string operations
 //         and the variables above so that the tests pass.
@@ -11,16 +10,17 @@ console.log(alphabet, firstName, secondName)
 //       NOT twoJanes = "JaneJane"
 
 // 1. Set this variable to firstName and secondName concatenated
-const fullName = null
+const fullName = firstName + " " + secondName
 
 // 2. Set this variable to the 10th character of the alphabet variable
-const tenthCharacterOfAlphabet = null
+const tenthCharacterOfAlphabet = alphabet.charAt(9)
 
 // 3. Set this variable by calling a method on the alphabet variable to transform it to lower case
-const lowerCaseAlphabet = null
+const lowerCaseAlphabet = alphabet.toLowerCase()
 
 // 4. Set this variable by using a property on the alphabet variable to get it's length
-const numberOfLettersInAlphabet = null
+const numberOfLettersInAlphabet = alphabet.length
+console.log(numberOfLettersInAlphabet)
 
 // do not edit the exported object.
 module.exports = {
