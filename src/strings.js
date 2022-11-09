e62386f94ea730d06d6c10113ec69479274c1442
Oliver West <oliversimon.west@gmail.com>
2022-11-09 15:44:47 +0000

    fixed typos

--- src/strings.js
+++ src/strings.js
@@ -3,14 +3,13 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const firstName = 'Jane'
 const secondName = 'Smith'
 
-
 // TODO: Replace null in the lines below using Javascript string operations
 //         and the variables above so that the tests pass.
 //       eg  twoJanes = firstName + firstName
 //       NOT twoJanes = "JaneJane"
 
 // 1. Set this variable to firstName and secondName concatenated
-const fullName = firstName + " " + secondName
+const fullName = firstName + ' ' + secondName
 
 // 2. Set this variable to the 10th character of the alphabet variable
 const tenthCharacterOfAlphabet = alphabet.charAt(9)
@@ -20,7 +19,6 @@ const lowerCaseAlphabet = alphabet.toLowerCase()
 
 // 4. Set this variable by using a property on the alphabet variable to get it's length
 const numberOfLettersInAlphabet = alphabet.length
-console.log(numberOfLettersInAlphabet)
 
 // do not edit the exported object.
 module.exports = {
