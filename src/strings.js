15784f4319f6811f0b882c54475f42c9b5e3f571
benji <benjipictor@gmail.com>
2022-11-09 16:41:14 +0000

    completed the second half of strings

--- src/strings.js
+++ src/strings.js
@@ -17,10 +17,12 @@ const tenthCharacterOfAlphabet = alphabet.charAt(9)
 console.log('2. The 10th letter of the alphabet is: ' + tenthCharacterOfAlphabet)
 
 // 3. Set this variable by calling a method on the alphabet variable to transform it to lower case
-const lowerCaseAlphabet = null
+const lowerCaseAlphabet = alphabet.toLowerCase()
+console.log('3. alphabet changed to lowercase: ' + lowerCaseAlphabet)
 
 // 4. Set this variable by using a property on the alphabet variable to get it's length
-const numberOfLettersInAlphabet = null
+const numberOfLettersInAlphabet = alphabet.length
+console.log('4. gives the number of charachters in the variable: ' +numberOfLettersInAlphabet)
 
 // do not edit the exported object.
 module.exports = {
