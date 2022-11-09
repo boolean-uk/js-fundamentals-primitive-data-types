ddfd6d2bcdb2f03099c11207f24bc94b93736895
Asiyeyurtkuran <asiyeyurtkuran@gmail.com>
2022-11-09 17:09:01 +0000

    numbers and answers have been changed

--- src/strings.js
+++ src/strings.js
@@ -9,7 +9,7 @@ const secondName = 'Smith'
 //       NOT twoJanes = "JaneJane"
 
 // 1. Set this variable to firstName and secondName concatenated
-const fullName = firstName + secondName
+const fullName = firstName + ' ' + secondName
 
 // 2. Set this variable to the 10th character of the alphabet variable
 const tenthCharacterOfAlphabet = alphabet.charAt(9)
@@ -18,7 +18,7 @@ const tenthCharacterOfAlphabet = alphabet.charAt(9)
 const lowerCaseAlphabet = alphabet.toLowerCase()
 
 // 4. Set this variable by using a property on the alphabet variable to get it's length
-const numberOfLettersInAlphabet = alphabet.length()
+const numberOfLettersInAlphabet = alphabet.length
 
 // do not edit the exported object.
 module.exports = {
