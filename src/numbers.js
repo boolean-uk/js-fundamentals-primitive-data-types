bdad583520d316441822d3d32e1849dd2551f26e
PanagiotisTziolos <panagiotistziolos@outlook.com>
2023-04-04 14:47:34 +0300

    completed exercise 2

--- src/numbers.js
+++ src/numbers.js
@@ -11,23 +11,33 @@ const numThree = 32
 //       eg  myAnswer = numOne * 42
 //       NOT myAnswer = 336
 
+console.log(numOne)
+console.log(numTwo)
+console.log(numThree)
+
 // 1. Set this variable to numOne added to numTwo
-const numOnePlusNumTwo = NaN
+const numOnePlusNumTwo = numOne + numTwo
+console.log("numOnePlusNumTwo : " + numOnePlusNumTwo)
 
 // 2. Set this variable to numThree multiplied by numTwo
-const numThreeTimesNumTwo = NaN
+const numThreeTimesNumTwo = numThree * numTwo
+console.log("numThreeTimesNumTwo : " + numThreeTimesNumTwo)
 
 // 3. Set this variable to numThree divided by numOne
-const numThreeDividedByNumOne = NaN
+const numThreeDividedByNumOne = numThree / numOne
+console.log("numThreeDividedByNumOne : " + numThreeDividedByNumOne)
 
 // 4. Set this variable to numThree minus numOne
-const numThreeMinusNumOne = NaN
+const numThreeMinusNumOne = numThree - numOne
+console.log("numThreeMinusNumOne : " + numThreeMinusNumOne)
 
 // 5. Set this variable to the sum of numOne, numTwo and numThree
-const sum = NaN
+const sum = numOne + numTwo + numThree
+console.log("sum : " + sum)
 
 // 6. Set this variable to the sum of (numOne, numTwo, numThree) divided by numOne
-const numBytes = NaN
+const numBytes = sum / numOne
+console.log("numBytes : " + numBytes)
 
 // do not edit the exported object.
 module.exports = {
