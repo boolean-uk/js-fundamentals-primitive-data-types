e562f74ba091391fdd63f3da9aeae63e24f83f2c
Chloe Zermatten <chloezermatten@gmail.com>
2023-08-31 17:15:18 +0200

    finally! - last question answered

--- src/strings.js
+++ src/strings.js
@@ -16,13 +16,13 @@ const secondName = 'Smith'
 const fullName = firstName + ' ' + secondName
 
 // 2. Set this variable to the 10th character of the alphabet variable
-const tenthCharacterOfAlphabet = alphabet.charAt(10)
+const tenthCharacterOfAlphabet = alphabet.charAt(9)
 
 // 3. Set this variable by calling a method on the alphabet variable to transform it to lower case
 const lowerCaseAlphabet = alphabet.toLowerCase()
 
-// 4. Set this variable by using a property on the alphabet variable to get it's length
-const numberOfLettersInAlphabet = null
+// 4. Set this variable by using a property on the alphabet variable to get its length
+const numberOfLettersInAlphabet = alphabet.length
 
 // do not edit the exported object.
 module.exports = {
