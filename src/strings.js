c38671035aaa3b60f9692bb140799bbf29257df4
meetpapi <65924157+meetpapi@users.noreply.github.com>
2023-09-01 18:04:23 +0200

    Revert "Papi"

--- src/strings.js
+++ src/strings.js
@@ -19,7 +19,7 @@ const fullName = 'Jane' + ' ' + 'Smith'
 const tenthCharacterOfAlphabet = 'J'
 
 // 3. Set this variable by calling a method on the alphabet variable to transform it to lower case
-const lowerCaseAlphabet = 'abcdefghijklmnopqrstuvwxyz'
+const lowerCaseAlphabet = alphabet.toLowerCase
 
 // 4. Set this variable by using a property on the alphabet variable to get it's length
 const numberOfLettersInAlphabet = 26
