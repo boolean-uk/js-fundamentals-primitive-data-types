7c1ea5915fb5e9ffffcbb19aad6b38c506faaa0d
Faiza <faraz_ul_islam@yahoo.com>
2023-09-12 13:59:01 +0100

    solved all

--- src/extensions/more-data-types.js
+++ src/extensions/more-data-types.js
@@ -10,7 +10,7 @@ const trueVariable = 'true'
 const falseVariable = 'false'
 
 // 3. Set this variable to be undefined
-const undefinedVariable = 'undefine'
+const undefinedVariable = 'undefined'
 
 // 4. get the typeof each of the above variables
 // hint you can use typeof variable to return a string of the variable type
