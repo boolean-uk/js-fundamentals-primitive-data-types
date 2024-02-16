c3d4192a1f81b8880c451fff76da74b4b33f135e
Klara Andersson <klara.andersson@se.experis.com>
2024-02-16 12:46:17 +0100

    Small fix

--- src/extensions/more-data-types.js
+++ src/extensions/more-data-types.js
@@ -14,9 +14,9 @@ const undefinedVariable = undefined
 
 // 4. get the typeof each of the above variables
 // hint you can use typeof variable to return a string of the variable type
-const typeOfTrueVariable = trueVariable.typeOfFalseVariable
-const typeOfFalseVariable = falseVariable.typeOfFalseVariable
-const typeOfUndefinedVariable = undefinedVariable.typeOfUndefinedVariable
+const typeOfTrueVariable = typeof trueVariable
+const typeOfFalseVariable = typeof falseVariable
+const typeOfUndefinedVariable = typeof undefinedVariable
 
 // do not edit the exported object.
 module.exports = {
