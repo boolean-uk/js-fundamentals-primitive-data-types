488b83a935a599587b1778d310b60ecc4fc86ba4
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-02-16 18:44:39 +0100

    All tasks have been done

--- src/strings.js
+++ src/strings.js
@@ -14,19 +14,15 @@ const secondName = 'Smith'
 
 // 1. Set this variable to firstName and secondName concatenated
 const fullName = firstName + ' ' + secondName
-console.log(fullName)
 
 // 2. Set this variable to the 10th character of the alphabet variable
 const tenthCharacterOfAlphabet = alphabet[9]
-console.log(tenthCharacterOfAlphabet)
 
 // 3. Set this variable by calling a method on the alphabet variable to transform it to lower case
 const lowerCaseAlphabet = alphabet.toLowerCase()
-console.log(lowerCaseAlphabet)
 
 // 4. Set this variable by using a property on the alphabet variable to get it's length
 const numberOfLettersInAlphabet = alphabet.length
-console.log(numberOfLettersInAlphabet)
 
 // do not edit the exported object.
 module.exports = {
