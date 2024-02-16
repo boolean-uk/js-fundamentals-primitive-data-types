d24f87e5c9fd30d4271b7b10842f1d4eb673527d
Santhia <santhia.shanmukarasan@gmail.com>
2024-02-16 12:38:04 +0100

    core,extensions and advanced completed

--- src/advanced/strings-adv.js
+++ src/advanced/strings-adv.js
@@ -3,25 +3,31 @@ const cityStr = 'City'
 const shoppingList = 'apples, bananas, oranges, grapes'
 
 // TODO get the INDEX of the character where 'York' word is found in the city variable
-const cityIndex = 0
+//The indexOf() method is used to find the index of the first occurrence of a specified substring ('York' in this case) within the string.
+const cityIndex = city.indexOf('York');
 
 // TODO - get the substring "York" from the city variable
-const citySubstring = ''
+/*city.substring(cityIndex, cityIndex + 4): This part of the line calls the substring() method on the city string.
+
+cityIndex: This is the starting index from where the substring extraction begins. In this case, it's the index where 'York' starts in the city string, as determined in the previous line.
+
+cityIndex + 4: This is the ending index of the substring extraction. Since 'York' is 4 characters long, we add 4 to cityIndex to get the ending index.*/
+const citySubstring = city.substring(cityIndex, cityIndex + 4);
 
 // TODO = replace 'York' with 'Delhi'
-const cityReplaced = ''
+const cityReplaced = city.replace('York', 'Delhi');
 
 // TODO - check if city starts with 'New' and ends with 'York'
-const cityStartsWith = false
-const cityEndsWith = false
+const cityStartsWith = city.startsWith('New');
+const cityEndsWith = city.endsWith('York');
 
 // TODO - split the shopping list into an array of items without spaces
 // hint (you might want to replace first, then split)
-const shoppingListArray = []
+const shoppingListArray = shoppingList.split(', ').map(item => item.trim());
 
 // TODO - concatenate two strings using the `${var}` syntax
 // make cityStrConcat equal to 'New York City'
-const cityStrConcat = ''
+const cityStrConcat = `${city} ${cityStr}`;
 
 module.exports = {
   cityIndex,
