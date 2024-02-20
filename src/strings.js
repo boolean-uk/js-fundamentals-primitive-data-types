f54eef1814bbc85893adfce2d39bae561b2cd4bf
Alexander Niklasson <alexander.niklasson@live.com>
2024-02-20 12:22:13 +0100

    exercise complete

--- src/strings.js
+++ src/strings.js
@@ -13,7 +13,7 @@ const secondName = 'Smith'
 //       NOT twoJanes = "JaneJane"
 
 // 1. Set this variable to firstName and secondName concatenated
-const fullName = firstName + ' ' +secondName
+const fullName = firstName + ' ' + secondName
 
 // 2. Set this variable to the 10th character of the alphabet variable
 const tenthCharacterOfAlphabet = alphabet[9]
