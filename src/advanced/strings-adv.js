e3b8d0fb27790d6e840e675e130015aa719d9371
Ateeb <ateeb@live.no>
2024-02-21 08:44:34 +0100

    hello

--- src/advanced/strings-adv.js
+++ src/advanced/strings-adv.js
@@ -1,27 +1,43 @@
 const city = 'New York'
-const cityStr = 'City'
 const shoppingList = 'apples, bananas, oranges, grapes'
 
 // TODO get the INDEX of the character where 'York' word is found in the city variable
-const cityIndex = 0
+const cityIndex = city.indexOf('Y')
 
 // TODO - get the substring "York" from the city variable
-const citySubstring = ''
+const citySubstring = city.slice(4, 8)
 
 // TODO = replace 'York' with 'Delhi'
-const cityReplaced = ''
+const cityReplaced = city.replace('York', 'Delhi')
 
 // TODO - check if city starts with 'New' and ends with 'York'
 const cityStartsWith = false
+if (city.startsWith('New')) {
+  // eslint-disable-next-line no-const-assign
+  cityStartsWith = true
+}
+
 const cityEndsWith = false
+if (city.endsWith('York')) {
+  // eslint-disable-next-line no-const-assign
+  cityStartsWith = true
+}
 
 // TODO - split the shopping list into an array of items without spaces
 // hint (you might want to replace first, then split)
 const shoppingListArray = []
 
+shoppingList.replace(' ', '')
+
+shoppingListArray.push(city.slice('apples'))
+shoppingListArray.push(city.slice('bananas'))
+shoppingListArray.push(city.slice('oranges'))
+shoppingListArray.push(city.slice('grapes'))
+
 // TODO - concatenate two strings using the `${var}` syntax
 // make cityStrConcat equal to 'New York City'
-const cityStrConcat = ''
+const aCity = 'New York City'
+const cityStrConcat = `${aCity}`
 
 module.exports = {
   cityIndex,
