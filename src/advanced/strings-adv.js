0ef86b2ee9666cbd1649004cd413d25c0988a4ce
Justinas <gercasjustinas@gmail.com>
2024-03-14 10:51:04 +0100

    fixed founded mistakes

--- src/advanced/strings-adv.js
+++ src/advanced/strings-adv.js
@@ -6,10 +6,10 @@ const shoppingList = 'apples, bananas, oranges, grapes'
 const cityIndex = city.indexOf('York')
 console.log(cityIndex)
 // TODO - get the substring "York" from the city variable
-const citySubstring = 'York'
+const citySubstring = city.substring(4)
 console.log(citySubstring)
 // TODO = replace 'York' with 'Delhi'
-const cityReplaced = 'New Delhi'
+const cityReplaced = city.replace('York', 'Delhi')
 console.log(cityReplaced)
 
 // TODO - check if city starts with 'New' and ends with 'York'
@@ -18,7 +18,7 @@ const cityEndsWith = city.endsWith('York')
 console.log(cityStartsWith, cityEndsWith)
 // TODO - split the shopping list into an array of items without spaces
 // hint (you might want to replace first, then split)
-const shoppingListArray = ['apples', 'bananas', 'oranges', 'grapes']
+const shoppingListArray = shoppingList.split(', ')
 console.log(shoppingListArray)
 // TODO - concatenate two strings using the `${var}` syntax
 // make cityStrConcat equal to 'New York City'
